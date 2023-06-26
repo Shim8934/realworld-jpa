d48e6dfd5f5c739d72ca9eb7ab958e866d586f97
ShimKi <shim8934@gmail.com>
2023-06-27 01:40:50 +0900

    first commit

---
+++ src/main/java/com/io/realworldjpa/RealworldJpaApplication.java
@@ -0,0 +1,13 @@
+package com.io.realworldjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealworldJpaApplication.class, args);
+    }
+
+}
